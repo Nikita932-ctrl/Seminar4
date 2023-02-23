ac5f723412779bd273f0859a1f99d59fb7722f1c
Nikita932-ctrl <65113631+Nikita932-ctrl@users.noreply.github.com>
2023-02-24 02:52:55 +0300

    Seminar4

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int degree (int a , int b) {
+    int c = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        
+        
+        c *= a;
+        Console.WriteLine($"{c}");
+
+    }
+    return c;
+
+}
+
+degree(3, 5);
+
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.Вариант 1
+
+
+
+int amount(int a)
+{
+    int result = 0;
+    if (a <= 99)
+    {
+        int firstIndex = a / 10;
+        int secondIndex = a % 10;
+        result = firstIndex + secondIndex;
+    }
+    else if (a >= 100 && a <= 999)
+    {
+
+        int middleIndex = a % 100 / 10;
+        int firstIndex = a / 100;
+        int lastIndex = a % 10;
+        result = middleIndex + firstIndex + lastIndex;
+    }
+    else if (a >= 1000 && a <= 9999)
+    {
+        int lastIndex = a % 10;
+        int preLastIndex = a % 100 / 10;
+        int middleIndex = a / 100 % 10;
+        int firstIndex = a / 1000;
+        result = middleIndex + firstIndex + lastIndex + preLastIndex;
+    }
+    return result;
+
+}
+
+Console.WriteLine(amount(999));
+
+
+
+
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] numberArray()
+{
+    
+    Console.Write("Введите количество элементов массива: ");
+    int n = int.Parse(Console.ReadLine());
+    int[] a = new int[n];
+
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = int.Parse(Console.ReadLine());
+
+    }
+    return a;
+}
+
+Console.WriteLine(String.Join(" ",numberArray()));
